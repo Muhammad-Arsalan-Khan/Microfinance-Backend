1250976180c72723fb9f55da773592c166d70380
arsalan-smit <arsalangg05@gmail.com>
2025-07-29 17:41:06 +0500

    temp handler

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-import app from '../app.js';
-import serverless from 'serverless-http';
+// import app from '../app.js';
+// import serverless from 'serverless-http';
 
-export const handler = serverless(app);
+// export const handler = serverless(app)
+
+export default function handler(req, res) {
+  res.status(200).json({ working: true });
+}
+
