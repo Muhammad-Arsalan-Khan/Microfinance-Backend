3edc11e0989da701ed495533f50cffe3d859568e
Muhammad Arsalan Khan <arsalangg05@gmail.com>
2025-08-01 02:55:30 +0500

    temp change app.js

--- app.js
+++ app.js
@@ -1,48 +1,63 @@
+// import express from 'express';
+// import dotenv from 'dotenv'
+// import cors from 'cors'
+// import cookieParser from 'cookie-parser';
+// import { connectMongoDB } from "./connection/db.js"
+// //import { logReqRes } from './middleware/log.js';
+// import MFroutes from "./routes/Routers.js"
+
+// dotenv.config();
+
+// const app = express()
+
+// //Middleware
+// //app.use(logReqRes("log.txt"))
+
+// const corsOptions = {
+//   origin: '*',   //http://localhost:5173
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH',], 
+//   credentials: true, 
+// };
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }))
+// app.use(cors(corsOptions))
+// app.use(cookieParser())
+
+// //database connection
+// //connectMongoDB().then(()=>{console.log("connect to MongoDB")})
+
+
+
+// //Route
+// app.use("/api", MFroutes);
+// app.use("/",(req, res) => res.json({msg: "server start"}));
+
+
+// const PORT = process.env.PORT || 5000
+// // app.listen(PORT, ()=> console.log(`server start at ${PORT}`))
+// connectMongoDB()
+//   .then(() => {
+//     console.log("MongoDB connected");
+
+//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+//   })
+//   .catch((err) => {
+//     console.error(" MongoDB connection failed:", err.message);
+//     process.exit(1)
+//   })
+
 import express from 'express';
-import dotenv from 'dotenv'
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
-import { connectMongoDB } from "./connection/db.js"
-//import { logReqRes } from './middleware/log.js';
-import MFroutes from "./routes/Routers.js"
 
-dotenv.config();
+const app = express();
 
-const app = express()
+app.get('/', (req, res) => {
+  res.json({ msg: "server running" });
+});
 
-//Middleware
-//app.use(logReqRes("log.txt"))
+const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: '*',   //http://localhost:5173
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH',], 
-  credentials: true, 
-};
+app.listen(PORT, () => {
+  console.log(`🚀 Basic test server running on port ${PORT}`);
+});
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-app.use(cors(corsOptions))
-app.use(cookieParser())
-
-//database connection
-//connectMongoDB().then(()=>{console.log("connect to MongoDB")})
-
-
-
-//Route
-app.use("/api", MFroutes);
-app.use("/",(req, res) => res.json({msg: "server start"}));
-
-
-const PORT = process.env.PORT || 5000
-// app.listen(PORT, ()=> console.log(`server start at ${PORT}`))
-connectMongoDB()
-  .then(() => {
-    console.log("MongoDB connected");
-
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-  })
-  .catch((err) => {
-    console.error(" MongoDB connection failed:", err.message);
-    process.exit(1)
-  });
